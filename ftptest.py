65f343daf6e963353b3cf16226ef67f0c4f19640
zcw <softzcw@hotmail.com>
2016-01-28 17:49:49 +0800

    change ftptest.py

--- ftptest.py
+++ ftptest.py
@@ -1,9 +1,9 @@
 #coding=utf-8
 import winrm
-username='biaogeuser'
-passwd='Ao7,*/$yRc8*IS4s^*'
-ftpname='lixiaoyang'
-ftppasswd='lxy(*23ec35'
+username='user'
+passwd='password'
+ftpname='li'
+ftppasswd='lxy'
 #dirname=raw_input("请输入你要下载的目录:")
 filename='testfile.py'
 f=open("d:\\ip.txt")
@@ -12,7 +12,7 @@ ip_list=f.readlines()
 for ip in ip_list:
 	s = winrm.Session(ip,auth=(username,passwd))
 	ftp_cmd='wget'
-	ftp_cmd_arg='-P'+' '+'d:\\'+' '+'ftp://'+ftpname+':'+ftppasswd+'@'+'117.79.91.177'+'/'+filename
+	ftp_cmd_arg='-P'+' '+'d:\\'+' '+'ftp://'+ftpname+':'+ftppasswd+'@'+'192.168.1.1'+'/'+filename
 	print ftp_cmd+ftp_cmd_arg
 	r = s.run_cmd(ftp_cmd,[ftp_cmd_arg])
 	print r.status_code
